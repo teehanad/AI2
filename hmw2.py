fa6255c992be6004c3162900dc4c034513fd8659
Adam Teehan <adamteehan@Adams-MacBook-Pro.local>
2019-04-02 21:27:48 +0100

    Fixed calculation error on unfit

--- hmw2.py
+++ hmw2.py
@@ -3,7 +3,7 @@ import os
 n = 0
 G = 0
 s = ''
-pExercise = [[0.891, 0.009, 0.1], [0.18, 0.77, 0.1], [0, 0, 1]]
+pExercise = [[0.891, 0.009, 0.1], [0.18, 0.72, 0.1], [0, 0, 1]]
 rExercise = [[8,8,0],[0,0,0],[0,0,0]]
 pRelax = [[0.693, 0.297, 0.01], [0, 0.99, 0.01], [0,0,1]]
 rRelax = [[10,10,0],[5,5,0],[0,0,0]]
@@ -13,7 +13,6 @@ rRelax = [[10,10,0],[5,5,0],[0,0,0]]
 
 
 def main():
-    os.system('clear')
     global n 
     n = input("Enter a value for n >= 0: ")
     while (int(n) < 0):
@@ -30,12 +29,11 @@ def main():
         G = input("Try Again: ")
 
     i = 0
-    print("The program isn't frozen its just a little slow as in order to print asnwers in order from 0 to n it calculates all previous answers for each iteration n times")
-    print("It will print finished when done")
+    print("-------------------------------------------------------------------(Calculating values from 0 to "+str(n)+"...)""-----------------------------------------------------------------")
     while i <= n:
-        print ("For n = " +str(i)+ "and G = " +str(G)+ "Exercise = "+str(q(i, s, 'Exercise'))+" "+"Relax = "+  str(q(i, s, 'Relax')))
+        print ("For n = " +str(i)+ " & G = " +str(G)+ " Exercise = "+str(q(i, s, 'Exercise'))+" "+"Relax = "+  str(q(i, s, 'Relax')))
         i+= 1
-    print('Finished')
+    print('--------------------------------------------------------------------------------Finished--------------------------------------------------------------------------------')
 
 
 
@@ -76,10 +74,10 @@ def r(s,a,sDash):
     elif (sDash == "Dead"):
         j = 2
     
-    if(a == "Relax"):
-        return rRelax[i][j]
-    else:
+    if(a == "Exercise"):
         return rExercise[i][j]
+    else:
+        return rRelax[i][j]
 
 def V(n,s):
     return max(q(n,s, 'Exercise'), q(n,s, 'Relax'))
